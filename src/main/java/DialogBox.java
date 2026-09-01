52e8feebd57c40fe304a132ca5659771d678801e
Sakthi <sakthi@Sakthi-Lenovo>
2026-09-01 18:09:20 +0800

    Created and layout GUI

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -6,12 +6,18 @@ import javafx.scene.layout.HBox;
 
 public class DialogBox extends HBox {
 
-    private Label text;
-    private ImageView displayPicture;
+  private Label text;
+  private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
-        this.getChildren().addAll(text, displayPicture);
-    }
+  public DialogBox(String s, Image i) {
+    text = new Label(s);
+    displayPicture = new ImageView(i);
+
+    text.setWrapText(true);
+    displayPicture.setFitWidth(100.0);
+    displayPicture.setFitHeight(100.0);
+    this.setAlignment(Pos.TOP_RIGHT);
+
+    this.getChildren().addAll(text, displayPicture);
+  }
 }
